9b1f5b4b57af3e81c851e7ace79eec875cbb4d88
RobertDS-45 <robertmwakamela2045@gmail.com>
2026-05-26 17:04:34 +0300

    deploymentsetup

--- LQS/settings.py
+++ LQS/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qca=qv&+(den2gklk222ba$+q1+8v)j+i8_-4afabj*z8xea&h'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-qca=qv&+(den2gklk222ba$+q1+8v)j+i8_-4afabj*z8xea&h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['Roby45.pythonanywhere.com']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 
 STATIC_URL = 'static/'
@@ -128,6 +129,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn LQS.wsgi --log-file -
